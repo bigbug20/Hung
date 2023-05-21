906b00afd54a27014814ae488da4b67ad506e1dd
Hung <bigbug0942@gmail.com>
2023-05-21 23:08:08 +0700

    Hung

--- a.py
+++ a.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 while True:
     try:
         usd = int(input("Nhap USD:"))
@@ -10,9 +9,5 @@ while True:
         print("Nhap sai vui long nhap lai so nguyen: ")
 
 vnd = usd * 22
-=======
-usd = int(input("Nhap USD: "))
-vnd = int(usd * 22)
->>>>>>> 393d2dc669a581a2af08416b786b2efb9eb250be
 
 print(str(usd) + " USD = " + str(vnd) + "k VND")
