f46a6da607e459a55fd12f42e8d1ac3ba9a2e9dd
Hung <bigbug0942@gmail.com>
2023-05-22 18:00:44 +0700

    Hung

--- data.py
+++ data.py
@@ -1,33 +1,25 @@
 class Student:
-    
+
     count = 0
-    
-    def __init__(self, id, name, birth):
+
+    def __init__(self , id , name):
         self.id = id
         self.name = name
-        self.birth = birth
         Student.count += 1
-        
-    def set_id(self, id):
+
+    def set_id(self , id):
         self.id = id
-    
+
     def get_id(self):
         return self.id
-    
-    def set_name(self, name):
+
+    def set_Name(self, name):
         self.name = name
-   
-    def get_name(self):
+
+    def get_Name(self):
         return self.name
-    
-    def set_birth(self, birth):
-        self.birth = birth
-   
-    def get_birth(self):
-        return self.birth
-    
+
     def show_info(self):
-        print(f"Id: {self.id}")
-        print(f"Ten: {self.name}")
-        print(f"Ngay sinh: {self.birth}")
-        
+        print(f"\nThông tin Sinh Viên :  \n")
+        print(f"Id :  { self.get_id() }")
+        print(f"Tên Sinh Viên :  { self.name }")
