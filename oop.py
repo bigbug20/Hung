38183326d801b96105b94cde86b255b2e00f5e14
Hung <bigbug0942@gmail.com>
2023-06-07 16:45:00 +0700

    first commit

--- oop.py
+++ oop.py
@@ -4,10 +4,12 @@ class Hung:
         self.body = para_body
         self.voiu = para_voiu
         
-    def embe(self):
-        return "Vợ iu của Hùng la:" + self.voiu
+class Hung2023(Hung):
+    def __init__(self, para_sinhnhat, para_body, para_voiu,para_nghe):
+        super().__init__(para_sinhnhat, para_body, para_voiu)
+        self.nghe = para_nghe
         
 
-NTH = Hung("28/10/2002" , "Đẹp trai 6 múi", "Hồng Ngọc Mặp" )
+NTH = Hung2023("28/10/2002" , "Đẹp trai 6 múi", "Hồng Ngọc Mặp","Backend Dev" )
 
-print(NTH.embe())
+print(NTH.__dict__)
